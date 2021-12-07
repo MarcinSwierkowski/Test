9672eab67a2c8b4483ab19ede459f0708b376bc4
marcin domowy <marcin@itcom.pl>
2021-12-07 18:07:23 +0100

    initial

---
+++ src/pl/sda/myproject/Main.java
@@ -0,0 +1,21 @@
+package pl.sda.myproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Ulamek pierwszy = new Ulamek(4,5);
+        Ulamek drugi = new Ulamek(2,6);
+
+        System.out.println(pierwszy.toString());
+        System.out.println(drugi.toString());
+
+        System.out.println(pierwszy.dodajUlamki(drugi).toString());
+        System.out.println(pierwszy.odejmijUlamki(drugi).toString());
+        System.out.println(pierwszy.pomnozUlamki(drugi).toString());
+        System.out.println(pierwszy.podzielUlamki(drugi).toString());
+
+        System.out.println(pierwszy.toString());
+        System.out.println(drugi.toString());
+    }
+}
