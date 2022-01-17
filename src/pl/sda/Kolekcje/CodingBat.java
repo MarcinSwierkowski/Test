16c36aad355169b22d71ba55f60884d9e44e8487
Marcin Swierkowski <marcin@itcom.pl>
2022-01-17 17:06:58 +0100

    kolekcje codingbat

--- src/pl/sda/Kolekcje/CodingBat.java
+++ src/pl/sda/Kolekcje/CodingBat.java
@@ -50,7 +50,7 @@ public class CodingBat {
     }
 
 
-    public Map<String, String> topping2(Map<String, String> map) {
+    public static Map<String, String> topping2(Map<String, String> map) {
         if (map.containsKey("ice cream")) {
             map.put("yogurt", map.get("ice cream"));
         }
@@ -62,7 +62,7 @@ public class CodingBat {
     }
 
 
-    public Map<String, String> topping3(Map<String, String> map) {
+    public static Map<String, String> topping3(Map<String, String> map) {
         if (map.containsKey("potato")) {
             map.put("fries", map.get("potato"));
         }
@@ -73,7 +73,7 @@ public class CodingBat {
         return map;
     }
 
-    public Map<String, String> mapAB2(Map<String, String> map) {
+    public static Map<String, String> mapAB2(Map<String, String> map) {
         if (map.containsKey("a") && map.containsKey("b")) {
             if (map.get("a").equals(map.get("b"))) {
                 map.remove("a");
@@ -83,5 +83,71 @@ public class CodingBat {
         return map;
     }
 
+    public static Map<String, String> mapAB3(Map<String, String> map) {
+        if (!(map.containsKey("a") && map.containsKey("b"))) {
+            if (map.containsKey("a")) map.put("b",map.get("a"));
+            if (map.containsKey("b")) map.put("a",map.get("b"));
+        }
+        return map;
+    }
+
+
+    public static Map<String, String> mapAB4(Map<String, String> map) {
+        if ((map.containsKey("a") && map.containsKey("b"))) {
+            if(map.get("a").length() == map.get("b").length())
+            {
+                map.put("a","");
+                map.put("b","");
+                return map;
+            }
+            if ( map.get("a").length()> map.get("b").length())
+                map.put("c",map.get("a"));
+            else map.put("c",map.get("b"));
+        }
+        return map;
+    }
+
+
+    public static Map<String, Integer> word0(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+        for (String s : strings)
+            map.put(s,0);
+        return map;
+    }
+
+    public static Map<String, Integer> wordLen(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+        for (String s : strings)
+            map.put(s,s.length());
+        return map;
+    }
+
+    public static Map<String, String> pairs(String[] strings) {
+        Map<String,String> map = new HashMap<>();
+        for (String s : strings)
+            map.put(s.substring(0,1),s.substring(s.length()-1,s.length()));
+        return map;
+    }
+
+    public static Map<String, Integer> wordCount(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+        for (String s : strings)
+            map.put(s,0);
+        for (String s : strings)
+            map.put(s,map.get(s)+1);
+        return map;
+    }
+
+    public static Map<String, String> firstChar(String[] strings) {
+        Map<String,String> map = new HashMap<>();
+        for (String s : strings)
+            map.put(s.substring(0,1),"");
+        for (String s : strings)
+            map.put(s.substring(0,1),map.get(s.substring(0,1))+s);
+        return map;
+    }
+
 
 }
+
+
