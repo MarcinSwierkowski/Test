91b3d85bb131a3ab7bdecfa7e64c74220c62fc98
marcin <marcin@itcom.pl>
2022-01-27 05:21:59 +0100

    io

--- src/pl/sda/Wątki/FirstThread.java
+++ src/pl/sda/Wątki/FirstThread.java
@@ -5,13 +5,22 @@ public class FirstThread {
     public static void main(String[] args) {
         System.out.println("MT start");
         Thread thread = new Thread(() -> {
-            System.out.println("T0 start");
+            System.out.println("T1 start");
             for (int i = 0; i < 5; i++) {
-                System.out.println("T0 " + i);
+                System.out.println("T1 " + i);
             }
-            System.out.println("T0 stop");
+            System.out.println("T1 stop");
         });
+        Thread thread2 = new Thread(() -> {
+            System.out.println("T2 start");
+            for (int i = 0; i < 5; i++) {
+                System.out.println("T2 " + i);
+            }
+            System.out.println("T2 stop");
+        });
+
         thread.start();
+        thread2.start();
         for (int i = 0; i < 5; i++) {
             System.out.println("MT " + i);
         }
--- src/pl/sda/Wątki/PoRunnable.java
+++ src/pl/sda/Wątki/PoRunnable.java
@@ -10,34 +10,46 @@ public class PoRunnable {
 //            Tak stworzony wątek startujemy za pomocą metody start.
 
 
-
-           App1 app = new App1();
-           App1 app1 = new App1();
+            Licznik counter = new Licznik(0);
+           App1 app = new App1(counter);
+           App1 app1 = new App1(counter);
            new Thread(app,"Wątek-1").start();
-           new Thread(app1,"Wątek-2").start();
+           new Thread(app1,"       Wątek-2").start();
 
-           new Thread(()->{
-               Thread.currentThread().setName("Wątek-3 z Lambdy");
-               System.out.println("wydruk z Lambdy");
-               System.out.println("Zakończenie bieżacego wątku o Id: "+Thread.currentThread().getId()+" ,name :"+Thread.currentThread().getName());
-           }).start();
+//           new Thread(()->{
+//               Thread.currentThread().setName("Wątek-3 z Lambdy");
+//               System.out.println("wydruk z Lambdy");
+//               System.out.println("Zakończenie bieżacego wątku o Id: "+Thread.currentThread().getId()+" ,name :"+Thread.currentThread().getName());
+//           }).start();
 
 
             System.out.println("Zakończenie bieżacego wątku o Id: "+Thread.currentThread().getId()+" ,name :"+Thread.currentThread().getName());
-            System.out.println(app);
-            System.out.println(app1);
+            System.out.println(counter.getIloscUruchomien()+" : "+ counter.getIloscUruchomien2());
         }
+
     }
 
     class App1 implements Runnable {
 
-        Licznik licznik = new Licznik();
+        private Licznik licznik ;
 
+
+        public App1(Licznik licznik) {
+            this.licznik = licznik;
+        }
 
         @Override
         public void run() {
             System.out.println("wydruk z Class App1");
-            licznik.zwiekszLiczbeUruchomien();
+            for (int i = 0; i < 100; i++) {
+                licznik.zwiekszLiczbeUruchomien();
+                licznik.zwiekszLiczbeUruchomien2();
+
+                for (int j = 0; j < Math.abs(Math.random() * 200); j++) {
+                }
+                System.out.println(Thread.currentThread().getName());
+            }
+
             System.out.println("Zakończenie bieżacego wątku o Id: "+Thread.currentThread().getId()+" ,name :"+Thread.currentThread().getName());
         }
 
@@ -45,12 +57,27 @@ public class PoRunnable {
     }
 
 
-class Licznik{
+final class Licznik{
 
- private int iloscUruchomien =0;
+ private int iloscUruchomien = 0;
+ private int iloscUruchomien2 = 0;
+
+    public Licznik(int iloscUruchomien) {
+        this.iloscUruchomien = iloscUruchomien;
+    }
+    public int getIloscUruchomien() {
+        return iloscUruchomien;
+    }
+
+    public int getIloscUruchomien2() {
+        return iloscUruchomien2;
+    }
 
     void zwiekszLiczbeUruchomien(){
         iloscUruchomien++;
     }
 
+    void zwiekszLiczbeUruchomien2(){
+        iloscUruchomien2++;
+    }
 }
